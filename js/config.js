5b979a46a00eb831252fbb0b465414b086226dac
Aravind Velaga <52666655+aravind-velaga@users.noreply.github.com>
2025-04-24 19:10:04 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_CXhTjskiX', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5jshvme5819hifb1a2g3e8sba8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_xaqaCiJB5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6n22cr238g5gbpmb93pqb853cf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
